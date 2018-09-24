9d2011c418e80fd050522e2aa87f5ad504a5031f
DmitryLenko <dias1381254@gmail.com>
2018-09-24 10:42:58 +0300

    added working prototype (16 tests failing)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,83 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.total = 0;
+    this.equal = [initialValue];
+   
   }
-
+ get recalculated() {
+    var arr = this.equal.slice();
+    console.log(arr)
+    for(let i = 0; i< arr.length; i++) {
+      if(arr[i] === '^') {
+        var res = Math.pow(arr[i-1] ,arr[i+1]);
+        arr.splice(i-1,3,res);
+        i=0;
+      }
+    }
+    for(let i = 0; i< arr.length; i++) {
+      if(arr[i] === '*') {
+        var res = arr[i-1] * arr[i+1];
+        arr.splice(i-1,3,res);
+        i=0;
+      }
+    }
+    for(let i = 0; i< arr.length; i++) {
+      if(arr[i] === '/') {
+        var res = arr[i-1] / arr[i+1];
+        arr.splice(i-1,3,res);
+        i=0;
+      }
+    }
+    
+    for(let i = 0; i< arr.length; i++) {
+      if(arr[i] === '+') {
+        var res = arr[i-1] + arr[i+1];
+        arr.splice(i-1,3,res);
+        i=0;
+      }
+      if(arr[i] === '-') {
+        var res = arr[i-1] - arr[i+1];
+        arr.splice(i-1,3,res);
+        i=0;
+      }
+      
+    }
+    
+    return arr[0];
+  }
+  
   add(number) {
-    // your implementation
+    this.equal.push('+');
+    this.equal.push(number);
+		return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.equal.push('-');
+    this.equal.push(number);
+		return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.equal.push('*');
+    this.equal.push(number);
+		return this;
   }
 
   devide(number) {
-    // your implementation
+    this.equal.push('/');
+    this.equal.push(number);
+		return this;
   }
 
   pow(number) {
-    // your implementation
+    this.equal.push('^');
+    this.equal.push(number);
+		return this;
+  }
+
+  valueOf(){
+    return this.recalculated;
   }
 }
 
